9df7220c56b79ef0805a4591de3b1aaa82d727d8
Rhys Evans <rhys.evans@ft.com>
2014-01-24 11:04:49 +0000

    nesting of partials from other modules

--- grunt-tasks/o-build-templates.js
+++ grunt-tasks/o-build-templates.js
@@ -1,21 +1,23 @@
 module.exports = function (grunt) {
+
+	function inlineOrigamiPartials (template, module) {
+		var newTemplate = grunt.file.read(template).replace(/\{\{ *> *(o\-[a-z\d\-]+)\/([\w\d\-]+)(?:\.mustache)? *\}\}/g, function ($0, module, template) {
+			return inlineOrigamiPartials('./bower_components/' + module + '/' + template + '.mustache', module);
+		});
+		if (module) {
+			grunt.file.write('origami-templates/' + module + '/' + template.split('/').pop(), newTemplate);
+		} else {
+			grunt.file.write('origami-templates/' + template, newTemplate);			
+		}
+		return newTemplate;
+	}
+
 	grunt.registerTask('o-build-templates', function () {
 		grunt.config.requires('o-build-templates.files');
 
 		var templates = grunt.config('o-build-templates.files');
 
-		templates.forEach(function (template) {
-			var newTemplate = grunt.file.read(template).replace(/\{\{ *> *(o\-[a-z\d\-]+)\/([\w\d\-]+)(?:\.mustache)? *\}\}/g, function ($0, module, template) {
-
-				return grunt.file.read('./bower_components/' + module + '/' + template + '.mustache');
-
-				//return 'origami-templates/' + module + '/' + template + '.mustache';
-
-
-			});
-
-			grunt.file.write('origami-templates/' + template, newTemplate);
-		});
+		templates.forEach(inlineOrigamiPartials);
 	});
 
 };
